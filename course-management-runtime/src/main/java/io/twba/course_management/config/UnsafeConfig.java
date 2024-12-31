cec040bac1e4552c8a52408e28651ea62a3940d2
David Cano <dcanoh78@gmail.com>
2024-12-31 12:03:57 +0100

    Inital configuration certs

---
+++ course-management-runtime/src/main/java/io/twba/course_management/config/UnsafeConfig.java
@@ -0,0 +1,24 @@
+package io.twba.course_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("unsafe")
+public class UnsafeConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
