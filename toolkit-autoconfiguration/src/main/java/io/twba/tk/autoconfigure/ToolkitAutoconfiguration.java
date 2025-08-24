4f896b942d378f5602bc3e7241f5d43aeabec293
David Cano <dcanoh78@gmail.com>
2025-08-24 20:03:22 +0200

    spring autoconfiguration for event store

---
+++ toolkit-autoconfiguration/src/main/java/io/twba/tk/autoconfigure/ToolkitAutoconfiguration.java
@@ -0,0 +1,30 @@
+package io.twba.tk.autoconfigure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.twba.tk.configure.ToolkitProperties;
+import io.twba.tk.eventsource.EventStore;
+import io.twba.tk.eventsource.EventStoreJdbcPostgres;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@EnableConfigurationProperties
+@Configuration
+public class ToolkitAutoconfiguration {
+
+    @ConfigurationProperties(prefix = "io.twba.tk.properties")
+    public ToolkitProperties toolkitProperties() {
+        return new ToolkitProperties();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "io.twba.tk.properties.event-sourcing", name = "type", havingValue = "postgres")
+    public EventStore jdbcPostgresEventStore(DataSource dataSource, ObjectMapper objectMapper) {
+        return new EventStoreJdbcPostgres(dataSource, objectMapper);
+    }
+
+}
--- toolkit-event-store-jdbc-postgres/src/main/java/io/twba/tk/eventsource/EventStoreJdbcPostgres.java
+++ toolkit-event-store-jdbc-postgres/src/main/java/io/twba/tk/eventsource/EventStoreJdbcPostgres.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-class EventStoreJdbcPostgres implements EventStore {
+public class EventStoreJdbcPostgres implements EventStore {
 
     private static final String INSERT_EVENT_SQL = """
         INSERT INTO event_sourcing_schema.event_store (
